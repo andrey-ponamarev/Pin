6c07e192e783d05b3705b49008b81f3e3cff29db
adreyponamarev <andrey.ponamarev@maxymiser.com>
2016-11-12 13:09:52 +0100

    save webpack

---
+++ test/webpackDevServer.conf.js
@@ -0,0 +1,22 @@
+var config = require('./../webpack.config.js');
+
+module.exports = {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    // It suppress error shown in console, so it has to be set to false.
+    quiet: false,
+    // It suppress everything except error, so it has to be set to false as well
+    // to see success build.
+    noInfo: false,
+    stats: {
+      // Config for minimal console.log mess.
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    }
+};
