eac752a47f9ef05975c1ac222fb9d15184950077
adreyponamarev <andrey.ponamarev@maxymiser.com>
2016-11-17 07:54:48 +0100

    package change

---
+++ app/src/styles/themes.js
@@ -0,0 +1,16 @@
+import {deepPurple500 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/colors
+const muiTheme = getMuiTheme({
+    palette: {
+        
+    },
+    appBar: {
+        height: 50,
+    },
+});
+
+export default muiTheme;
